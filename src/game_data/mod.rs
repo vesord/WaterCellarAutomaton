5d1d6d33fee7d2db4e282aeafdf09b3078e12a01
Matrus <matrus@student.21-school.ru>
2021-10-09 10:15:04 +0300

    controls refactor

--- src/game_data/mod.rs
+++ src/game_data/mod.rs
@@ -4,9 +4,9 @@ use gl_render::uniform::HasUniform;
 use resources::Resources;
 use surface::Surface;
 use crate::camera::MVP;
-use controls::{Actions, Controls};
+use controls::{Controls};
 use grid::{Grid, GridingAlgo};
-use water::{Water, Direction};
+use water::{Water};
 
 pub mod controls;
 mod surface;
@@ -64,18 +64,6 @@ impl GameData {
         self.apply_uniforms().map_err(err_msg)
     }
 
-    pub fn process_input(&mut self) -> Result<(), failure::Error> {
-        if self.controls.exit.into() { self.action_exit() };
-        if self.controls.flush.into() { self.action_flush() };
-        if self.controls.add_water.into() { self.action_add_water() };        // TODO: rename inc_water / dec_water
-        if self.controls.wave_n.into() { self.action_wave_n() };
-        if self.controls.wave_s.into() { self.action_wave_s() };
-        if self.controls.wave_w.into() { self.action_wave_w() };
-        if self.controls.wave_e.into() { self.action_wave_e() };
-        if self.controls.cam_capture.into() { self.action_cam_capture().map_err(err_msg)? };
-        Ok(())
-    }
-
     pub fn render(&self) {
         self.color_buffer.clear(&self.gl);
         self.surface.render(&self.gl, gl::TRIANGLES); // TODO: add key for changing render mode
@@ -108,61 +96,5 @@ impl GameData {
             self.gl.DepthRange(0., 1.);
             self.gl.ClearDepth(1.);
         }
-    }
-
-    fn action_flush(&mut self) {
-        println!("FLUSH!");
-        self.controls.reset_action(Actions::Flush);
-        self.water.flush();
-    }
-
-    fn action_add_water(&mut self) {
-        println!("ADD WATER");
-        self.controls.reset_action(Actions::AddWater);
-        self.water.increase_water_level();
-    }
-
-    fn action_wave_s(&mut self) {
-        println!("WAVE SOUTH");
-        self.controls.reset_action(Actions::WaveS);
-        self.water.add_wave_particles(Direction::South);
-    }
-
-    fn action_wave_n(&mut self) {
-        println!("WAVE NORTH");
-        self.controls.reset_action(Actions::WaveN);
-        self.water.add_wave_particles(Direction::North);
-    }
-
-    fn action_wave_e(&mut self) {
-        println!("WAVE EAST");
-        self.controls.reset_action(Actions::WaveE);
-        self.water.add_wave_particles(Direction::East);
-    }
-
-    fn action_wave_w(&mut self) {
-        println!("WAVE WEST");
-        self.controls.reset_action(Actions::WaveW);
-        self.water.add_wave_particles(Direction::West);
-    }
-
-    fn action_cam_capture(&mut self) -> Result<(), failure::Error> {
-        let naviball: na::Vector2<i32> = self.controls.get_naviball();
-        self.controls.save_mouse_clk_pos();
-        if naviball.x == 0 && naviball.y == 0 {
-            return Ok(())
-        }
-
-        let naviball: na::Vector2<f32> = na::Vector2::new(
-            (naviball.x) as f32 / (self.viewport.w) as f32,
-            (naviball.y) as f32 / (self.viewport.h) as f32 );
-
-        self.mvp.view_rotate_naviball(naviball);
-        self.apply_uniforms().map_err(err_msg)?;
-        Ok(())
-    }
-
-    fn action_exit(&mut self) {
-        self.need_exit = true;
     }
 }
